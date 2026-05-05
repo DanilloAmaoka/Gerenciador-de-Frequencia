72b6b34238c104e3b5bb088d65a0c0b2d3692a85
DanilloAmaoka <danillosilvaamaoka28@gmail.com>
2026-05-05 19:15:23 -0300

    reformado o design

--- src/pages/Creditos.jsx
+++ src/pages/Creditos.jsx
@@ -9,6 +9,7 @@ function Creditos() {
       <h3>ícone de Educação apresentação palestra estudante by Icogenix on <a href="https://icon-icons.com/pt/authors/1377-icogenix">Icon-Icons.com</a></h3>
       <h3>Edit user student staff Icon by Alfredo Creates on <a href="https://icon-icons.com/authors/244-alfredo-creates">Icon-Icons.com</a></h3>
       <h3>Tool configuration option service Icon by graficon on <a href="https://icon-icons.com/authors/1124-graficon">Icon-Icons.com</a></h3>
+      <h3>Back button Icon by Rami McMin on <a href="https://icon-icons.com/authors/605-rami-mcmin">Icon-Icons.com</a></h3>
     </div>
   );
 }
