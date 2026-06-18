29c9dfca334b4267a0241b0c0a6594a8f8e76619
DanilloAmaoka <danillosilvaamaoka28@gmail.com>
2026-06-18 10:11:40 -0300

    Reajuste na Proteção de Rotas

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,18 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ProtectedRoute } from '../components/ProtectedRoute';
 
 import Login from '../pages/Login';
 import Inicio from '../pages/Inicio';
 import Turmas from '../pages/Turmas';
-import TurCadastrarFaltas from '../pages/CadastrarFaltas';
+import CadastrarFaltas from '../pages/CadastrarFaltas';
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/inicio" element={<Inicio />} />
-        <Route path="/turmas" element={<Turmas />} />
-        <Route path="/cadastrarfaltas" element={<CadastrarFaltas />} />
+        <Route path="/" element={<ProtectedRoute><Login /></ProtectedRoute>} />
+        <Route path="/inicio" element={<ProtectedRoute><Inicio /></ProtectedRoute>} />
+        <Route path="/turmas" element={<ProtectedRoute><Turmas /></ProtectedRoute>} />
+        <Route path="/cadastrarfaltas" element={<ProtectedRoute><CadastrarFaltas /></ProtectedRoute>} />
       </Routes>
     </BrowserRouter>
   );
